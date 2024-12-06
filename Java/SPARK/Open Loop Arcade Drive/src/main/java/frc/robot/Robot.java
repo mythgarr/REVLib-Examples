9b4cd410b6cc7fa8ed96b324dd9ecf1b4a2bbfd5
Jan-Felix Abellera <jfabellera@gmail.com>
2024-12-05 18:06:04 -0600

    [java] Add Open Loop Arcade Drive example

---
+++ Java/SPARK/Open Loop Arcade Drive/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,142 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Robot extends TimedRobot {
+  SparkMax leftLeader;
+  SparkMax leftFollower;
+  SparkMax rightLeader;
+  SparkMax rightFollower;
+  XboxController joystick;
+
+  public Robot() {
+    // Initialize the SPARKs
+    leftLeader = new SparkMax(1, MotorType.kBrushless);
+    leftFollower = new SparkMax(2, MotorType.kBrushless);
+    rightLeader = new SparkMax(3, MotorType.kBrushless);
+    rightFollower = new SparkMax(4, MotorType.kBrushless);
+
+    /*
+     * Create new SPARK MAX configuration objects. These will store the
+     * configuration parameters for the SPARK MAXes that we will set below.
+     */
+    SparkMaxConfig globalConfig = new SparkMaxConfig();
+    SparkMaxConfig rightLeaderConfig = new SparkMaxConfig();
+    SparkMaxConfig leftFollowerConfig = new SparkMaxConfig();
+    SparkMaxConfig rightFollowerConfig = new SparkMaxConfig();
+
+    /*
+     * Set parameters that will apply to all SPARKs. We will also use this as
+     * the left leader config.
+     */
+    globalConfig
+        .smartCurrentLimit(50)
+        .idleMode(IdleMode.kBrake);
+
+    // Apply the global config and invert since it is on the opposite side
+    rightLeaderConfig
+        .apply(globalConfig)
+        .inverted(true);
+
+    // Apply the global config and set the leader SPARK for follower mode
+    leftFollowerConfig
+        .apply(globalConfig)
+        .follow(leftLeader);
+
+    // Apply the global config and set the leader SPARK for follower mode
+    rightFollowerConfig
+        .apply(globalConfig)
+        .follow(rightLeader);
+
+    /*
+     * Apply the configuration to the SPARKs.
+     *
+     * kResetSafeParameters is used to get the SPARK MAX to a known state. This
+     * is useful in case the SPARK MAX is replaced.
+     *
+     * kPersistParameters is used to ensure the configuration is not lost when
+     * the SPARK MAX loses power. This is useful for power cycles that may occur
+     * mid-operation.
+     */
+    leftLeader.configure(globalConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    leftFollower.configure(leftFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    rightLeader.configure(rightLeaderConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    rightFollower.configure(rightFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    // Initialize joystick
+    joystick = new XboxController(0);
+  }
+
+  @Override
+  public void robotPeriodic() {
+    // Display the applied output of the left and right side onto the dashboard
+    SmartDashboard.putNumber("Left Out", leftLeader.getAppliedOutput());
+    SmartDashboard.putNumber("Right Out", rightLeader.getAppliedOutput());
+  }
+
+  @Override
+  public void autonomousInit() {
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+  }
+
+  @Override
+  public void teleopInit() {
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    /**
+     * Get forward and rotation values from the joystick. Invert the joystick's
+     * Y value because its forward direction is negative.
+     */
+    double forward = -joystick.getLeftY();
+    double rotation = joystick.getRightX();
+
+    /*
+     * Apply values to left and right side. We will only need to set the leaders
+     * since the other motors are in follower mode.
+     */
+    leftLeader.set(forward + rotation);
+    rightLeader.set(forward - rotation);
+  }
+
+  @Override
+  public void disabledInit() {
+  }
+
+  @Override
+  public void disabledPeriodic() {
+  }
+
+  @Override
+  public void testInit() {
+  }
+
+  @Override
+  public void testPeriodic() {
+  }
+
+  @Override
+  public void simulationInit() {
+  }
+
+  @Override
+  public void simulationPeriodic() {
+  }
+}
